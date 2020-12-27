015e5b82bea4fb501d5458bd01508e35b3c05d8c
Alexander Arrico <alex.arrico@gmail.com>
2020-12-27 15:42:08 -0800

    create food class

---
+++ models.py
@@ -0,0 +1,7 @@
+class Food():
+
+    def __init__(self, name: str, protein: float, carbs: float, fat: float):
+        self.name = name
+        self.protein = protein
+        self.carbs = carbs
+        self.fat = fat
