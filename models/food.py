4db2a72814420cf147089a3b37bd7ee9450c4d0e
Alexander Arrico <alex.arrico@gmail.com>
2021-01-03 17:29:05 -0800

    add flask restful get, delete, and put methods to food class, lay groundwork for db methods to be written

--- models/food.py
+++ models/food.py
@@ -1,4 +1,14 @@
-class Food:
+from typing import Dict
+
+from flask_restful import Resource, abort
+
+
+def abort_if_food_doesnt_exist(food_name: str):
+    if db.does_resource_exist("food", food_name):
+        abort(404, message="{} doesn't exist".format(food_name))
+
+
+class Food(Resource):
     def __init__(self, data: Dict) -> None:
         self.name: str = data["name"]
         self.protein: float = data["protein"]
@@ -22,8 +32,23 @@ class Food:
     def __repr__(self):
         return f"Food(\
                 name={self.name}, \
-                protein={self.country}, \
-                carbs={self.population}, \
-                fat={self.capital}, \
-                servingType={self.regions}\
+                protein={self.protein}, \
+                carbs={self.carbs}, \
+                fat={self.fat}, \
+                servingType={self.serving_type}\
             )"
+
+    def get(self, food_name: str):
+        abort_if_food_doesnt_exist(food_name)
+        return Food(db.get("food", food_name))
+
+    def delete(self, food_name: str):
+        abort_if_food_doesnt_exist(food_name)
+        db.delete("food", food_name)
+        return "", 204
+
+    def put(self, food_name: str):
+        args = parser.parse_args()
+        food = {"food": args["food"]}
+        db.put("food", food_name)
+        return food, 201
